183d1734d54a00f661c0ba5bc84e2cf65a1a9010
lolowu95 <641812032@qq.com>
2025-03-20 10:00:52 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,7 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js';
-import { getDatabase, ref, set, get, remove, onValue } from 'https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js';
-import { getAuth, signInAnonymously } from 'https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js';
+// 引入 Firebase SDK
+import firebase from 'https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js';
+import 'https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js';
+import 'https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js';
 
 // 用户提供的 Firebase 配置
 const firebaseConfig = {
@@ -14,17 +15,28 @@ const firebaseConfig = {
 };
 
 // 初始化 Firebase
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-const auth = getAuth(app);
+const app = firebase.initializeApp(firebaseConfig);
+const database = firebase.database(app);
+const auth = firebase.auth(app);
 
 // 匿名登录
-signInAnonymously(auth)
+auth.signInAnonymously()
     .then(() => {
         console.log("Signed in anonymously to Firebase");
     })
     .catch((error) => {
         console.error("Anonymous auth failed:", error.code, error.message);
     });
+
+// 导出 Firebase Realtime Database 方法
+const ref = (path) => database.ref(path);
+const set = (ref, value) => ref.set(value);
+const get = (ref) => ref.once('value').then(snapshot => snapshot);
+const remove = (ref) => ref.remove();
+const onValue = (ref, callback) => {
+    ref.on('value', (snapshot) => {
+        callback(snapshot);
+    });
+};
 
 export { database, ref, set, get, remove, onValue };
